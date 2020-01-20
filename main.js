5f3cb58c648ecabf464548d42b2c7a26408aa85f
Daniele Lanzi <dnlshiva@gmail.com>
2020-01-20 09:33:32 +0100

    esercizio completato

---
+++ main.js
@@ -0,0 +1,10 @@
+// <===========================INIZIO========================>
+
+$('.fas.fa-bars').on('click', function() {
+  $('.hamburger-menu').show();
+});
+$('.fas.fa-times').on('click', function() {
+  $('.hamburger-menu').hide();
+});
+
+// <===========================FINE========================>
